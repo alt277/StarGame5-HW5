a90dc5edb4b273e9067367dac0ab96a1671e8d69
Alexey Kutepov <alkutepov@gmail.com>
2020-02-17 20:01:22 +0300

    Операции с векторами

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -5,29 +5,50 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	TextureRegion region;
+	private SpriteBatch batch;
+	private Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 20, 25, 150, 100);
-	}
+
+		Vector2 v1 = new Vector2(1, 1);
+		Vector2 v2 = new Vector2(2, 2);
+		v1.add(v2);
+		System.out.println("v1 add v2 v1.x=" + v1.x + " v1.y=" + v1.y);
+
+		v1.set(3, 2);
+		v2.set(5, 6);
+		v2.sub(v1);
+		System.out.println("v2 add v1 v2.x=" + v2.x + " v2.y=" + v2.y);
+		System.out.println("v2.len()=" + v2.len());
+
+		v1.set(10, 0);
+		System.out.println("v1.len()=" + v1.len());
+		v1.scl(0.9f);
+		System.out.println("v1.len()=" + v1.len());
+		System.out.println("v1.scl(0.9f); v1.x=" + v1.x + " v1.y=" + v1.y);
+
+		v1.nor();
+		System.out.println("v1.len()=" + v1.len());
+
+		v1.set(13, 15);
+		v2.set(-1, 18);
+		v1.nor();
+		v2.nor();
+		System.out.println(Math.acos(v1.dot(v2)));
+}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0.5f, 0.9f, 0.4f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.setColor(0.1f, 0.9f, 0.5f, 0.9f);
 		batch.draw(img, 0, 0);
-		batch.setColor(0.6f, 0.3f, 0.2f, 0.3f);
-		batch.draw(region, 200, 200);
 		batch.end();
 	}
 	
