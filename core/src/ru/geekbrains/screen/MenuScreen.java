26a42ffed1da3eb927d34520107733f4c025521f
Alexey Kutepov <alkutepov@gmail.com>
2020-02-20 19:22:26 +0300

    ДЗ 2

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -9,18 +9,22 @@ import ru.geekbrains.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final float V_LEN = 1f;
+
     private Texture img;
     private Vector2 touch;
     private Vector2 v;
     private Vector2 pos;
+    private Vector2 buf;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
         touch = new Vector2();
-        v = new Vector2(1f,1f);
+        v = new Vector2();
         pos = new Vector2();
+        buf = new Vector2();
     }
 
     @Override
@@ -28,8 +32,11 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
         Gdx.gl.glClearColor(0.5f, 0.9f, 0.4f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        if ((pos.y + img.getHeight()) < Gdx.graphics.getHeight()) {
+        buf.set(touch);
+        if (buf.sub(pos).len() > V_LEN) {
             pos.add(v);
+        } else {
+            pos.set(touch);
         }
         batch.begin();
         batch.draw(img, pos.x, pos.y);
@@ -46,6 +53,7 @@ public class MenuScreen extends BaseScreen {
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         super.touchDown(screenX, screenY, pointer, button);
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos).setLength(V_LEN));
         System.out.println("touch.x = " + touch.x + " touch.y " + touch.y);
         return false;
     }
