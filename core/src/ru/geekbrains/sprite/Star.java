6414ac9c4854ab534aa701d0eba793ad995e6592
Alexey Kutepov <alkutepov@gmail.com>
2020-02-24 20:39:38 +0300

    Урок 4

---
+++ core/src/ru/geekbrains/sprite/Star.java
@@ -0,0 +1,46 @@
+package ru.geekbrains.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.Sprite;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.math.Rnd;
+
+public class Star extends Sprite {
+
+    private final Vector2 v;
+    private Rect worldBounds;
+
+    public Star(TextureAtlas atlas) {
+        super(atlas.findRegion("star"));
+        v = new Vector2();
+        v.set(Rnd.nextFloat(-0.005f, 0.005f), Rnd.nextFloat(-0.2f, -0.01f));
+    }
+
+    @Override
+    public void update(float delta) {
+        pos.mulAdd(v, delta);
+        if (getRight() < worldBounds.getLeft()) {
+            setLeft(worldBounds.getRight());
+        }
+        if (getLeft() > worldBounds.getRight()) {
+            setRight(worldBounds.getLeft());
+        }
+        if (getTop() < worldBounds.getBottom()) {
+            setBottom(worldBounds.getTop());
+        }
+        if (getBottom() > worldBounds.getTop()) {
+            setTop(worldBounds.getBottom());
+        }
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(0.01f);
+        float posX = Rnd.nextFloat(worldBounds.getLeft(), worldBounds.getRight());
+        float posY = Rnd.nextFloat(worldBounds.getBottom(), worldBounds.getTop());
+        pos.set(posX, posY);
+        this.worldBounds = worldBounds;
+    }
+}
